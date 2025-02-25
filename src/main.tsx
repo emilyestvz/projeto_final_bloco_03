cb0c1f0c5480d411bb650dbbe4de9c5361274330
Emily <emilys@genstudents.org>
2025-02-25 08:39:52 -0300

    :sparkles: Commit inicial

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
